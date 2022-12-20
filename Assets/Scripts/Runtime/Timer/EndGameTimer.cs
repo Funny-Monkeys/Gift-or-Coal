d39ca5377c7a3559ddd1f462024f0fb100262187
AtennopVer <anton7185569@gmail.com>
2022-12-20 21:44:19 +0300

    Added TimerView

--- Assets/Scripts/Runtime/Timer/EndGameTimer.cs
+++ Assets/Scripts/Runtime/Timer/EndGameTimer.cs
@@ -7,6 +7,7 @@ namespace GiftOrCoal.Timer
     {
         [SerializeField, Min(0.1f)] private float _sessionTime = 180f;
         [SerializeField] private EndGamePanel _endGamePanel;
+        [SerializeField] private TimerView _timerView;
         
         private readonly IGameLoop _gameLoop = new GameLoop.GameLoop();
         private float _time;
@@ -14,13 +15,14 @@ namespace GiftOrCoal.Timer
         private void Update()
         {
             _time += Time.deltaTime;
+            _timerView.Display((int)(_sessionTime - _time));
 
-            if (_time >= _sessionTime)
-            {
-                _time = 0;
-                _endGamePanel.Enable();
-                _gameLoop.Pause();
-            }
+            if (!(_time >= _sessionTime)) 
+                return;
+            
+            _time = 0;
+            _endGamePanel.Enable();
+            _gameLoop.Pause();
         }
     }
 }
