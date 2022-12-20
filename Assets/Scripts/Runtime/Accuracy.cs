e3d70d2108c66d5a315da370d69fe9b6f3d02cf8
Farid <Blackwazz7@gmail.com>
2022-12-20 14:51:11 +0500

    Done Accuracy

---
+++ Assets/Scripts/Runtime/Accuracy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using GiftOrCoal.View;
+using UnityEngine;
+
+namespace GiftOrCoal
+{
+    public sealed class Accuracy : MonoBehaviour
+    {
+        [SerializeField] private CountView _countView;
+        private int _mistakesCount;
+        private int _totalAnswersCount;
+
+        private void Start()
+        {
+            AddAnswers(2);
+            AddMistakes(1);
+        }
+
+        public void AddAnswers(int count)
+        {
+            if (count <= 0)
+                throw new ArgumentOutOfRangeException(nameof(count));
+
+            _totalAnswersCount += count;
+            VisualizeInPercents();
+        }
+
+        public void AddMistakes(int count)
+        {
+            if (count <= 0)
+                throw new ArgumentOutOfRangeException(nameof(count));
+            
+            _mistakesCount += count;
+            VisualizeInPercents();
+        }
+
+        private void VisualizeInPercents()
+        {
+            const float toPercents = 100f;
+            var percents = _mistakesCount / (float)_totalAnswersCount * toPercents;
+            _countView.Visualize(percents);
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/CountView.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using TMPro;
+using UnityEngine;
+
+namespace GiftOrCoal.View
+{
+    public sealed class CountView : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _text;
+        [SerializeField] private string _additionSymbols;
+
+        public void Visualize(float count)
+        {
+            var countText = count.ToString(CultureInfo.InvariantCulture);
+            _text.text = _additionSymbols.Length == 0 ? countText : countText + _additionSymbols;
+        }
+    }
+}
