1547ea1c80f819031760c8c29ebf9b034fa98339
Farid <Blackwazz7@gmail.com>
2022-12-23 22:48:06 +0500

    Added Christmas Tree

---
+++ Assets/Scripts/Runtime/Factories/SantaItems/SantaItemsFactory.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace GiftOrCoal.Dossier
+{
+    public sealed class SantaItemsFactory : MonoBehaviour
+    {
+        [SerializeField] private SantaItem _coalPrefab;
+        [SerializeField] private SantaItem _giftPrefab;
+        [SerializeField] private Transform _spawnPosition;
+        
+        public void CreateCoal(int count)
+        {
+            CreateItems(_coalPrefab, count);
+        }
+        
+        public void CreateGift(int count)
+        {
+            CreateItems(_giftPrefab, count);
+        }
+
+        private void CreateItems(SantaItem santaItemPrefab, int count)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                Instantiate(santaItemPrefab, _spawnPosition.position, Quaternion.identity);
+            }
+        }
+    }
+}
--- Assets/Scripts/Runtime/GameLoop/EndGamePanel.cs
+++ Assets/Scripts/Runtime/GameLoop/EndGamePanel.cs
@@ -4,9 +4,12 @@ namespace GiftOrCoal.Timer
 {
     public sealed class EndGamePanel : MonoBehaviour
     {
+        [SerializeField] private ChristmasTree _christmasTree;
+        
         public void Enable()
         {
             gameObject.SetActive(true);
+            _christmasTree.Blink();
         }
     }
 }
