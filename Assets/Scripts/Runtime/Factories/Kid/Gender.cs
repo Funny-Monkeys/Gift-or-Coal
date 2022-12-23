3e551cd6db70d4ae1ce50152ec861c500d238cb0
Farid <Blackwazz7@gmail.com>
2022-12-23 23:23:28 +0500

    Gender

---
+++ Assets/Scripts/Runtime/Factories/Kid/Gender.cs
@@ -0,0 +1,8 @@
+﻿namespace GiftOrCoal.KidData
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
--- Assets/Scripts/Runtime/KidData/IKid.cs
+++ Assets/Scripts/Runtime/KidData/IKid.cs
@@ -6,6 +6,7 @@ namespace GiftOrCoal.KidData
     public interface IKid
     {
         KidData Data { get; }
+        
         IEnumerable<Deed> Deeds { get; }
     }
 }
