3baa9f0b8b4d17151d9a008edbd329b9c4ec3928
Farid <Blackwazz7@gmail.com>
2022-12-26 00:38:08 +0500

    Update RandomDossierFactory.cs

--- Assets/Scripts/Runtime/Factories/Kid/RandomDossierFactory.cs
+++ Assets/Scripts/Runtime/Factories/Kid/RandomDossierFactory.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using UnityEngine.Localization.Settings;
 using Random = UnityEngine.Random;
 
 namespace GiftOrCoal.Factories.Kid
@@ -18,12 +19,12 @@ namespace GiftOrCoal.Factories.Kid
         {
             _notUsedHobbies = new List<string>(_hobbies);
             var stringBuilder = new StringBuilder();
-            stringBuilder.Append($"Hello, Santa Claus, my name is {kidName}. ");
-            
+            stringBuilder.Append(LocalizationSettings.SelectedLocale.LocaleName.Contains("English") ? $"Hello, Santa Claus, my name is  {kidName}. " : $"Привет, Санта Клаус, меня зовут{kidName}. ");
             var hobbies = BuildHobbies();
-            stringBuilder.Append($"I love {hobbies}. ");
-            stringBuilder.Append($"{_endsOfGreetings[Random.Range(0, _endsOfGreetings.Count)]}. This year I have done these things:\n");
-            
+            stringBuilder.Append(LocalizationSettings.SelectedLocale.LocaleName.Contains("English") ? $"I love {hobbies}. " : $"Я люблю {hobbies}. ");
+            stringBuilder.Append(LocalizationSettings.SelectedLocale.LocaleName.Contains("English")
+                ? $"{_endsOfGreetings[Random.Range(0, _endsOfGreetings.Count)]}. This year I have done these things:\n"
+                : $"{_endsOfGreetings[Random.Range(0, _endsOfGreetings.Count)]}. В этом году я:\n");
             return stringBuilder.ToString();
         }
         
@@ -38,12 +39,13 @@ namespace GiftOrCoal.Factories.Kid
             }
 
             hobbies = hobbies.Distinct().ToList();
+            var and = LocalizationSettings.SelectedLocale.LocaleName.Contains("English") ? "and" : "и";
             
             return hobbies.Count switch
             {
                 1 => BuildHobby(),
-                2 => $"{BuildHobby()} and {BuildHobby()}",
-                3 => $"{BuildHobby()}, {BuildHobby()} and {BuildHobby()}",
+                2 => $"{BuildHobby()} {and} {BuildHobby()}",
+                3 => $"{BuildHobby()}, {BuildHobby()} {and} {BuildHobby()}",
                 _ => throw new ArgumentOutOfRangeException($"HobbiesCount", "Too much hobbies")
             };
         }
