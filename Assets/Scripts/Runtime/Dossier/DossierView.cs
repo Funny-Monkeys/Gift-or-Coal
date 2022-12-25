d91a24b7873cc0a3340163220c33f4b9211f4680
Farid <Blackwazz7@gmail.com>
2022-12-26 00:24:56 +0500

    Bad And Good Deeds Localization

--- Assets/Scripts/Runtime/Dossier/DossierView.cs
+++ Assets/Scripts/Runtime/Dossier/DossierView.cs
@@ -1,7 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using GiftOrCoal.KidData;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Tables;
 using UnityEngine.UI;
 
 namespace GiftOrCoal.Dossier
@@ -10,21 +14,34 @@ namespace GiftOrCoal.Dossier
     {
         [SerializeField] private TextMeshProUGUI _dossierText;
         [SerializeField] private Image _kidPhoto;
-        [SerializeField] private TMP_Text _deedTextPrefab;
+        [SerializeField] private LocalizationText _deedTextPrefab;
         [SerializeField] private Transform _content;
-
+        [SerializeField] private List<LocalizedString> _localizedStrings;
+        
         public IKid CurrentKid { get; private set; }
 
         public void Display(IKid kid)
         {
             ClearContent();
             CurrentKid = kid;
+          //  var stringReference = _localizedStrings.Find(text => text.ToString().Contains(kid.Data.Dossier));
+            //_dossierLocalizeStringEvent.StringReference = stringReference;
             _dossierText.text = kid.Data.Dossier;
 
             for (var i = 0; i < kid.Deeds.Count(); i++)
             {
                 var deedText = Instantiate(_deedTextPrefab, _content);
-                deedText.text = $"{i + 1}) {kid.Deeds.ToList()[i].Text}";
+                var text = kid.Deeds.ToList()[i].Text;
+                
+                try
+                {
+                    var localizedString = _localizedStrings.Find(line => line.ToString().Contains(text));
+                    deedText.LocalizeStringEvent.StringReference = localizedString;
+                    text = localizedString.Values.ElementAt(0).ToString();
+                    deedText.Text.text = text;
+                }
+                
+                catch{}
             }
 
             _kidPhoto.sprite = kid.Data.Photo;
---
+++ Assets/Scripts/Runtime/Tools/LocalizationText.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Localization.Components;
+
+namespace GiftOrCoal.Dossier
+{
+    public sealed class LocalizationText : MonoBehaviour
+    {
+        [field: SerializeField] public TMP_Text Text { get; private set; }
+        
+        [field: SerializeField] public LocalizeStringEvent LocalizeStringEvent { get; private set; }
+
+        
+    }
+}
