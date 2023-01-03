e321f71c368d02735405adbebf92f1e81b8ff0ac
Farid <Blackwazz7@gmail.com>
2023-01-03 21:09:11 +0500

    Added SDK

--- Assets/Scripts/Runtime/Buttons/RestartButton.cs
+++ Assets/Scripts/Runtime/Buttons/RestartButton.cs
@@ -1,20 +1,43 @@
-﻿using GiftOrCoal.GameLoop;
+﻿using System;
+using GiftOrCoal.GameLoop;
 using GiftOrCoal.LoadSystem;
 using UnityEngine;
 
 namespace GiftOrCoal.Buttons
 {
-    public class RestartButton : Button
+    public sealed class RestartButton : Button
     {
         [SerializeField] private SceneData _sceneData;
         [SerializeField] private SceneLoader _sceneLoader;
         
+        private YandexSDK _yandexSDK;
         private readonly IGameLoop _gameLoop = new GameLoop.GameLoop();
+
+        private void Awake()
+        {
+            _yandexSDK = YandexSDK.instance;
+
+            if (_yandexSDK == null)
+                throw new ArgumentNullException(nameof(YandexSDK));
+
+            _yandexSDK.onInterstitialShown += OnShownInterstitial;
+        }
         
-        protected override void OnClick()
+        private void OnDestroy()
+        {
+            _yandexSDK.onInterstitialShown -= OnShownInterstitial;
+        }
+
+        private void OnShownInterstitial()
         {
             _gameLoop.Continue();
             _sceneLoader.Load(_sceneData);
+        }
+
+        protected override void OnClick()
+        {
+            _yandexSDK.ShowInterstitial();
+            _gameLoop.Pause();
         }
     }
 }
